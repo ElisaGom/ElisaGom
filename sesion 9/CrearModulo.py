01ccbc0372a17eb3b99138bc1a28d7dd73108d69
ElisaGom <142262833+ElisaGom@users.noreply.github.com>
2023-08-21 16:28:14 -0600

    Add files via upload

---
+++ sesion 9/CrearModulo.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import math\n",
+    "raiz = math.sqrt(4)\n",
+    "print('La raiz cuadrada es:', raiz)"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
---
+++ sesion 9/testSesion9.py
@@ -0,0 +1,12 @@
+import librarys.mixmodulo as mx
+
+archivo = "Power.csv"
+
+carga_datos = mx.cargar_datos(archivo)
+print(carga_datos.head())
+
+
+lst = [1, 2, 3, 4, 5]
+prom = mx.calcular_promedio(lst)
+
+print('EL promedio es : ', prom)
